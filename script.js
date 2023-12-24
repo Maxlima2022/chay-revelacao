73b9101e174a575f413d00bb759c37d6f4e53f21
Maxlima2022 <137231151+Maxlima2022@users.noreply.github.com>
2023-12-24 13:00:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+function submitConfirmation() {
+    var name = document.getElementById("name").value;
+    var attendance = document.getElementById("attendance").value;
+
+    var message = "Obrigado, " + name + "! Sua presença foi confirmada.";
+
+    if (attendance === "nao") {
+        message = "Que pena que você não poderá comparecer, " + name + ". Esperamos vê-lo na próxima!";
+    }
+
+    document.getElementById("confirmationMessage").innerHTML = message;
+}
+
+function sendWhatsAppMessage() {
+    var name = document.getElementById("name").value;
+    var attendance = document.getElementById("attendance").value;
+
+    var message = "Obrigado, " + name + "! Sua presença foi confirmada.";
+
+    if (attendance === "nao") {
+        message = "Que pena que você não poderá comparecer, " + name + ". Esperamos vê-lo na próxima!";
+    }
+
+    // Substitua "SEU_NUMERO_DE_TELEFONE" pelo número de telefone do destinatário
+    var phoneNumber = "551195834631";
+    
+    // Construa o link do WhatsApp
+    var whatsappLink = "https://wa.me/" + phoneNumber + "/?text=" + encodeURIComponent(message);
+
+    // Abra o WhatsApp no navegador
+    window.open(whatsappLink, "_blank");
+}
